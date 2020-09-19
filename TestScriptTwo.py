43f78bea00a96a17bfe2bdbdd6c1559a09fa9ddf
Angad Sarpotdar <sixteefour@gmail.com>
2020-09-19 14:15:15 +0100

    Initial commit

---
+++ TestScriptTwo.py
@@ -0,0 +1,14 @@
+# Simple Snake Game in Python 3 for beginners
+# By @TokyoEdTech
+# Part 1: Getting started
+
+import turtle
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake game")
+wn.bgcolor("green")
+wn.setupwidth(=600, height=)
+wn.tracer(0)
+
+wn.mainloop()
